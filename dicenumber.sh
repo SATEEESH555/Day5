2a3cd9605b090f19d3c1cebc8c9cfac1edf92d0d
sateeesh555 <88261800+SATEEESH555@users.noreply.github.com>
2023-04-27 20:50:54 +0530

    Add files via upload

---
+++ dicenumber.sh
@@ -0,0 +1,2 @@
+dicenum=$((RANDOM%7))
+  echo "The dice number is $dicenum"
---
+++ feets.sh
@@ -0,0 +1,6 @@
+
+feet=$((60))
+inches=$((12))
+totalinches=$((feet/inche))
+  echo "Toayl inches: $totalbash: line 1: shell.sh: command not found
+inches"
